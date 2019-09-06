e60ae3c591cc78f45515e38bf5b9c5a1aaefac4c
“Ostyk” <michalnarbutt@gmail.com>
2019-09-06 14:22:07 +0000

    gen fix

--- code/models.py
+++ code/models.py
@@ -22,40 +22,67 @@ class MFS(object):
     def __init__(self):
         pass
     
-class Basic(object):
-    """
-    Word Sense Disambiguiation performed via a basic sequence tagging
-    """
-    def __init__(self, test):
-        self.test = test
+# class Basic(object):
+#     """
+#     Word Sense Disambiguiation performed via a basic sequence tagging
+#     """
+#     def __init__(self, test):
+#         self.test = test
     
-    def build(self, vocab_size, embedding_size, hidden_size, PADDING_SIZE, LEARNING_RATE, INPUT_DROPOUT, LSTM_DROPOUT,RECURRENT_DROPOUT, N_EPOCHS):
+#     def build(self, vocab_size, embedding_size, hidden_size, PADDING_SIZE, LEARNING_RATE, INPUT_DROPOUT, LSTM_DROPOUT,RECURRENT_DROPOUT, N_EPOCHS):
         
-        print("Creating KERAS model")
-        inputs = K.layers.Input(shape=(PADDING_SIZE,))
-        embeddings = K.layers.Embedding(vocab_size,
-                                        embedding_size,
-                                        mask_zero=True,
-                                        name = 'embedding')(inputs)
+#         print("Creating KERAS model")
+#         inputs = K.layers.Input(shape=(PADDING_SIZE,))
+#         embeddings = K.layers.Embedding(vocab_size,
+#                                         embedding_size,
+#                                         mask_zero=True,
+#                                         name = 'embedding')(inputs)
 
 
-        BI_LSTM = (K.layers.Bidirectional(
-                   K.layers.LSTM(hidden_size, dropout = LSTM_DROPOUT,
-                                 recurrent_dropout = RECURRENT_DROPOUT,
-                                 return_sequences=True,
-                                 kernel_regularizer=K.regularizers.l2(0.01),
-                                 activity_regularizer=K.regularizers.l1(0.01)
-                                ), name = 'Bi-directional_LSTM'))(embeddings)
+#         BI_LSTM = (K.layers.Bidirectional(
+#                    K.layers.LSTM(hidden_size, dropout = LSTM_DROPOUT,
+#                                  recurrent_dropout = RECURRENT_DROPOUT,
+#                                  return_sequences=True,
+#                                  kernel_regularizer=K.regularizers.l2(0.01),
+#                                  activity_regularizer=K.regularizers.l1(0.01)
+#                                 ), name = 'Bi-directional_LSTM'))(embeddings)
 
-        predictions = K.layers.TimeDistributed(K.layers.Dense(vocab_size, activation='softmax'))(BI_LSTM)
+#         predictions = K.layers.TimeDistributed(K.layers.Dense(vocab_size, activation='softmax'))(BI_LSTM)
 
-        model = K.models.Model(inputs=[inputs], outputs=predictions)
+#         model = K.models.Model(inputs=[inputs], outputs=predictions)
 
-        model.compile(loss = 'sparse_categorical_crossentropy',
-                      optimizer = K.optimizers.Adam(lr=LEARNING_RATE, decay = 0.001/N_EPOCHS, amsgrad=False),
-                      metrics = ['acc'])
+#         model.compile(loss = 'sparse_categorical_crossentropy',
+#                       optimizer = K.optimizers.Adam(lr=LEARNING_RATE, decay = 0.001/N_EPOCHS, amsgrad=False),
+#                       metrics = ['acc'])
 
-        return model
+#         return model
+
+def Basic(vocab_size, embedding_size, hidden_size, PADDING_SIZE, LEARNING_RATE, INPUT_DROPOUT, LSTM_DROPOUT,RECURRENT_DROPOUT, N_EPOCHS):
+    print("Creating KERAS model")
+    inputs = K.layers.Input(shape=(PADDING_SIZE,))
+    embeddings = K.layers.Embedding(vocab_size,
+                                    embedding_size,
+                                    mask_zero=True,
+                                    name = 'embedding')(inputs)
+
+
+    BI_LSTM = (K.layers.Bidirectional(
+               K.layers.LSTM(hidden_size, dropout = LSTM_DROPOUT,
+                             recurrent_dropout = RECURRENT_DROPOUT,
+                             return_sequences=True,
+                             kernel_regularizer=K.regularizers.l2(0.01),
+                             activity_regularizer=K.regularizers.l1(0.01)
+                            ), name = 'Bi-directional_LSTM'))(embeddings)
+
+    predictions = K.layers.TimeDistributed(K.layers.Dense(vocab_size, activation='softmax'))(BI_LSTM)
+
+    model = K.models.Model(inputs=[inputs], outputs=predictions)
+
+    model.compile(loss = 'sparse_categorical_crossentropy',
+                  optimizer = K.optimizers.Adam(lr=LEARNING_RATE, decay = 0.001/N_EPOCHS, amsgrad=False),
+                  metrics = ['acc'])
+
+    return model
     
     
 class MultiTask(object):
